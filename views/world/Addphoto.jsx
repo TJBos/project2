e06933943ff19faf757d46f9a2676c9ca783bc67
Toon Bosmans <toon.j.bosmans@gmail.com>
2021-01-11 11:34:55 -0600

    added readme

--- views/world/Addphoto.jsx
+++ views/world/Addphoto.jsx
@@ -1,27 +1,25 @@
 const React = require('react');
 const Layout = require('../layout.jsx');
 
-class New extends React.Component {
+class AddPhoto extends React.Component {
     render() {
         const { country } = this.props;
         return (
             <Layout>
                 <div>
                     <h1>Add a photo</h1>
-                    <form action={`/world/${country._id}/`} method='POST'>
-                        <div className='form-group'>
-                        <label>
-                            Name <input type="text" className='form-control' placeholder="image url" name="photo" />
-                        </label>
-                        </div>
-                        <input className="btn btn-default" type="submit" value="Add Photo"></input>
-                        
+                    <form action={`/world/${country._id}`} method='POST'>
+                    <div className='form-group'>
+                    <label>
+                        Name <input type="text" className='form-control' placeholder="image url" name="photo" />
+                    </label>
+                    </div>
+                    <input className="btn btn-default" type="submit" value="Add Photo"></input>
                     </form>
-
                 </div>
             </Layout>
         );
     }
 }
 
-module.exports = New;
+module.exports = AddPhoto;
